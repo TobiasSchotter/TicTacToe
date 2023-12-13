0c15c50974683eed4c9ff0016265e22ab20f94f1
Tobias Schotter <tschotter98@gmx.de>
2023-12-13 14:36:08 +0100

    Initial commit

---
+++ Assets/Code/UI/MainMenu.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public void Play3d()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void Play2d()
+    {
+    }
+
+    public void GoToMainMenu()
+    {
+        SceneManager.LoadScene("main");
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
